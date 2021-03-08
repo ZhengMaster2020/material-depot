eb4fc2f71597c613d5658746e1c7a76ecfceb55d
lizheng <lizheng@668dingtalk.com>
2021-03-08 18:41:43 +0800

    change: edit deploy

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
 npm run docs:build
 
+
+
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+echo 'zhengmaster.org.cn' > CNAME
 
 git init
 git add -A
