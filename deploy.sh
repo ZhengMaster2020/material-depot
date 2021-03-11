f018e03c77469bfd5fc025c0bdaa073514a2831a
lizheng <lizheng@668dingtalk.com>
2021-03-11 08:50:18 +0800

    change: edit deploy config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env sh
-# 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
@@ -18,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:ZhengMaster2020/ZhengMaster2020.github.io.git master 
+git push -f git@github.com:ZhengMaster2020/blog-vuepress.git master 
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,29 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-02 11:01:28
+ * @LastEditTime: 2021-03-11 08:42:13
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \ZhengMaster2020.github.io\docs\.vuepress\config.js
+ */
 module.exports = {
   title: '资料库',
   description:
     '这是一个关于书籍的资料库， 前后端以及各种领域文学的知识、资料分享库',
   base: '/',
+  dest: '/',
+  head: [['link', { rel: 'icon', href: '../assets/images/book-pro.png' }]],
   plugins: ['@vuepress/blog', '@vuepress/back-to-top'],
 
   // 主题配置
   themeConfig: {
-    // logo: '/assets/images/demos.png',
-    repo: 'ZhengMaster2020/books',
+    repo: 'ZhengMaster2020/material-depot',
+    repoLabel: '查看源码',
     editLinks: true,
     editLinkText: '在 GitHub 上编辑此页',
     docsDir: 'docs',
     lastUpdated: 'Last Updated',
+    smoothScroll: true,
 
     displayAllHeaders: true,
     nav: [
